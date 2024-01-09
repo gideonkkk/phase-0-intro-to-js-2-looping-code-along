750d540962c8f11b40eedba7868d0361985517f9
Gideon Kariuki <gideon.kariuki@student.moringa.com>
2024-01-09 09:43:49 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+function writeCards(names,eventName){
+    const message = []
+    for (let i=0;i<names.length; i++){
+      message.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return message;
+}
+function countDown(number) {
+    if (number < 1) {
+      console.log("Please provide a positive integer.");
+      return;
+    }
+  
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+}// Code your solutions in this file
